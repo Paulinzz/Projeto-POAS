70383af6f6cbc76d395394046968c98378af9fde
ZagZx <azevedodvictor@gmail.com>
2026-06-12 23:11:02 -0300

    feat: campos para foto de perfil, retorno de data de criação e refatoração para Pydantic

--- backend/schemas/usuario.py
+++ backend/schemas/usuario.py
@@ -1,19 +1,29 @@
-from sqlmodel import SQLModel, Field
-from pydantic import EmailStr
+from pydantic import (
+    EmailStr, 
+    Field, 
+    BaseModel, 
+    Base64Bytes, 
+    FileUrl
+)
 from typing import Optional
+from datetime import datetime
 
 
-class UsuarioCreate(SQLModel):
+class UsuarioCreate(BaseModel):
     nome: str = Field(min_length=2, max_length=50)
     email: EmailStr = Field(max_length=150)
     senha: str = Field(min_length=8)
+    foto_perfil: Optional[Base64Bytes | FileUrl] = Field(default=None)
 
-class UsuarioRead(SQLModel):
+class UsuarioRead(BaseModel):
     id: int
     nome: str
     email: EmailStr
+    foto_perfil_url: Optional[FileUrl]
+    data_criacao: datetime
 
-class UsuarioUpdate(SQLModel):
+class UsuarioUpdate(BaseModel):
     nome: Optional[str] = Field(default=None, min_length=3, max_length=50)
-    email: Optional[EmailStr] = Field(default=None, max_length=150)
+    email: Optional[EmailStr] = Field(default=None)
     senha: Optional[str] = Field(default=None, min_length=8)
+    foto_perfil: Optional[Base64Bytes | FileUrl] = Field(default=None)
